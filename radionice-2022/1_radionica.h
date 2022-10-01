8c22f4f67bf2ce79a99377512f1eb03c644a791b
Petra-Robotika <114669949+Petra-Robotika@users.noreply.github.com>
2022-10-01 09:47:11 +0200

    Update 1_radionica.h

--- radionice-2022/1_radionica.h
+++ radionice-2022/1_radionica.h
@@ -1,6 +1,29 @@
 #pragma once
 #include "helper.h"
 
-void RobotLine::radionica() {
+void RobotLine::radionica(){
+// ZADATAK: Prikaži naziv robota.
+// HINT: display(); 
+display("PETRA");
+
+
+// ZADATAK: Neka robot opiše četverokut. 
+go(50, 50);
+ delayMs(1500);
+go(-60, 60);
+ delayMs(900);
+go(50, 50);
+ delayMs(1500);
+go(-60, 60);
+ delayMs(900);
+go(50, 50);
+ delayMs(1500);
+go(-60, 60);
+ delayMs(900);
+go(50, 50);
+
+
+stop(); 
+end();
 
 }
